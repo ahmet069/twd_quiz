b60109ec5c38b32d10cd9b3b5b9f2a42dcfffae9
Ahmet Muhammet Subaşı <ahmet.subasi@kodzillaistanbul.com>
2023-05-27 22:31:53 +0300

    added new colors

--- lib/src/config/color/app_color.dart
+++ lib/src/config/color/app_color.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color transparentWhite = Color.fromARGB(213, 255, 255, 255);
   static const Color buttonGreen = Color(0xD451DA4E);
+  static const Color darkBlue = Color(0xFF27374D);
+  static const Color lightBlue = Color(0xff9DB2BF);
+
 }
