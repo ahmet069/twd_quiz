43726c2f32bea0b2c28d4224274198315e64cd62
Ahmet Muhammet Subaşı <ahmet.subasi@kodzillaistanbul.com>
2023-05-28 14:54:50 +0300

    build

--- app_build.sh
+++ app_build.sh
@@ -1,2 +1,1 @@
-flutter build appbundle  --build-name=1.0.1 --build-number=1
-flutter build appbundle --no-tree-shake-icons --build-name=1.0.8 --build-number=8
+flutter build appbundle --no-tree-shake-icons --build-name=1.0.9 --build-number=9
